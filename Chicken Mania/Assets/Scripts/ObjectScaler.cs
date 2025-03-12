9f447cb348936a73cf7fe33921edcd3800f35f61
handmacewan <121985474+handmacewan@users.noreply.github.com>
2025-03-11 22:43:16 -0600

    Scaler with public variables

--- Chicken Mania/Assets/Scripts/ObjectScaler.cs
+++ Chicken Mania/Assets/Scripts/ObjectScaler.cs
@@ -5,6 +5,8 @@ public class ObjectScaler : MonoBehaviour
 {
     private TransformGesture scaleGesture;
     private Vector3 initialScale;
+    public float maxSize = 2f;
+    public float minSize = 1f;
 
     private void OnEnable()
     {
@@ -15,7 +17,7 @@ public class ObjectScaler : MonoBehaviour
     }
     private void Scaling(object sender, System.EventArgs e)
     {
-        float newScaleFactor = Mathf.Clamp(transform.localScale.x * scaleGesture.DeltaScale, 1f, 2f);
+        float newScaleFactor = Mathf.Clamp(transform.localScale.x * scaleGesture.DeltaScale, minSize, maxSize);
         transform.localScale = initialScale * newScaleFactor;
     }
     private void LateUpdate()
